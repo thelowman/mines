d5fff2a25d8541cdb3182e29b4d1fc85d845c5b9
Doug <doug@thelowman.com>
2020-12-24 15:38:06 -0500

    Working out gameplay.

--- mines.js
+++ mines.js
@@ -53,11 +53,16 @@ const mines = () => {
   let mines;
   const overlay = document.createElement('div');
   const gameStart = document.createElement('div');
+  const statusBoard = document.createElement('div');
   const gameGrid = document.createElement('div');
 
   overlay.classList.add('overlay');
   gameStart.classList.add('gameStart');
   gameGrid.classList.add('gameGrid');
+
+  overlay.addEventListener('click', e => pause());
+  gameStart.addEventListener('click', e => { e.preventDefault(); e.stopPropagation(); });
+  gameGrid.addEventListener('click', e => { e.preventDefault(); e.stopPropagation(); });
 
   const applySkin = name => {
     const skin = name ? name : basic;
@@ -70,7 +75,8 @@ const mines = () => {
       if (isNaN(elem.dataset.height)) console.warn(TE_NOSTARTHEIGHT);
       else h = parseInt(elem.dataset.height);
       elem.addEventListener('click', e => start(w, h));
-    })
+    });
+    statusBoard.innerHTML = skin.statusBoardHTML;
   }
 
   const pause = () => {
@@ -86,12 +92,18 @@ const mines = () => {
   }
 
   const reset = () => {
-    while(gameGrid.firstChild) gameGrid.removeChild(gameGrid.firstChild);
-    overlay.appendChild(gameStart);
-    setTimeout(() => gameStart.classList.add('shown'), 100);
+    gameGrid.classList.remove('shown');
+    setTimeout(() => {
+      while(gameGrid.firstChild) gameGrid.removeChild(gameGrid.firstChild);
+      mines = null;
+      overlay.removeChild(gameGrid);
+      overlay.appendChild(gameStart);
+      setTimeout(() => gameStart.classList.add('shown'), 100);
+    }, 500);
   }
 
   const start = (w, h) => {
+    gameGrid.appendChild(statusBoard);
     const grid = createGrid(w, h, (r, rI) => {
       const row = createElem(gameGrid, null, null, 'row');
       return (c, cI) => ({
@@ -130,12 +142,29 @@ const mines = () => {
           reveal(c.x, c.y)
         });
       }
+      if (remainingCells() === 0) win();
     }
+
+    const remainingCells = () => grid.reduce((t, row) => t += row.reduce((t2, cell) => 
+      cell.elem.classList.contains('hidden') && cell.value < 9 ?
+        t2 += 1 : t2
+      , 0), 0);
+
+    const marked = () => grid.reduce((t, row) => t += row.reduce((t2, cell) => 
+      cell.elem.classList.contains('marked') ?
+        t2 += 1 : t2
+      , 0), 0);
+
+    const remainingUnmarked = () => grid.reduce((t, row) => t += row.reduce((t2, cell) => 
+      cell.value === 9 && !cell.elem.classList.contains('marked') ?
+        t2 += 1 : t2
+      , 0), 0);
 
     const mark = (x, y) => {
       const cell = byCoord(x, y);
       if (!cell.elem.classList.contains('marked')) cell.elem.classList.add('marked');
       else cell.elem.classList.remove('marked');
+      if (remainingUnmarked() === 0) win();
     }
 
     // first click places mines
@@ -151,6 +180,10 @@ const mines = () => {
       reveal(x, y);
     }
 
+    const win = () => {
+      reset();
+    }
+
     const boom = (x, y) => {
       const unmarked = mines.reduce((f, m) => {
         const cell = byIndex(m);
@@ -162,8 +195,7 @@ const mines = () => {
         setTimeout(() => c.cell.elem.classList.add('boom'), i * 20);
       });
       setTimeout(() => {
-        gameGrid.classList.remove('shown');
-        setTimeout(() => reset(), 500);
+        reset();
       }, unmarked.length * 20 + 1000);
     }
   }
@@ -176,73 +208,6 @@ const mines = () => {
     overlay.classList.add('shown');
     setTimeout(() => gameStart.classList.add('shown'), 500);
   }, 100);
-
-
-  // const baseElem = document.createElement('div');
-  // overlay.appendChild(baseElem);
-
-  // const reveal = (x, y) => {
-  //   const cell = byCoord(x, y);
-  //   if (!cell.elem.classList.contains('hidden') || 
-  //     cell.elem.classList.contains('marked')) return;
-
-  //   cell.elem.classList.remove('hidden');
-  //   if (cell.value) {
-  //     if (cell.value === 9) boom(cell.x, cell.y);
-  //     else cell.elem.innerText = cell.value;
-  //   }
-  //   else {
-  //     border(cell.x, cell.y).forEach(i => {
-  //       var c = byIndex(i);
-  //       reveal(c.x, c.y)
-  //     });
-  //   }
-  // }
-
-  // const mark = (x, y) => {
-  //   const cell = byCoord(x, y);
-  //   if (!cell.elem.classList.contains('marked')) cell.elem.classList.add('marked');
-  //   else cell.elem.classList.remove('marked');
-  // }
-
-  // const boom = (x, y) => {
-  //   const unmarked = mines.reduce((f, m) => {
-  //     const cell = byIndex(m);
-  //     if (!cell.elem.classList.contains('marked'))
-  //       return f.concat({ cell, dist: Math.sqrt(Math.pow(cell.x - x, 2) + Math.pow(cell.y - y, 2)) })
-  //     return f;
-  //   }, []).sort((a, b) => a.dist > b.dist ? 1 : a.dist < b.dist ? -1 : 0).forEach((c, i) => {
-  //     setTimeout(() => c.cell.elem.classList.add('boom'), i * 20)
-  //   });
-  // }
-
-  // first click places mines
-  // const click = (x, y) => {
-  //   if (!mines) mines = generateMines(w, h, 0.16, x, y);
-  //   for(let i = 0; i < w * h; i++) {
-  //     let cell = byIndex(i);
-  //     if (mines.indexOf(cell.index) > -1) cell.value = 9;
-  //     else cell
-  //       .value = border(cell.x, cell.y)
-  //       .reduce((v, c) => mines.indexOf(c) > -1 ? v += 1 : v, 0);
-  //   }
-  //   reveal(x, y);
-  // }
-
-  // const grid = createGrid(w, h, (r, rI) => {
-  //   const row = createElem(baseElem, null, null, 'row');
-  //   return (c, cI) => ({
-  //     y: rI,
-  //     x: cI,
-  //     index: rI * w + cI,
-  //     value: 0,
-  //     elem: createElem(row, () => click(cI, rI), () => mark(cI, rI), 'cell', 'hidden')
-  //   });
-  // });
-
-  // const byIndex = cell_i(w, h, grid);
-  // const byCoord = cell_xy(w, h, grid);
-  // const border = border_i(w, h);
 
   return { pause, resume }
 };
@@ -272,5 +237,12 @@ const basic = {
         </button>
       </div>
     </div>
-    `
+    `,
+  statusBoardHTML: `
+    <div class="statusBoard">
+      <div data-value="time">Time</div>
+      <div data-value="marked">Marked</div>
+      <div data-value="total">Total</div>
+    </div>
+  `
 };
